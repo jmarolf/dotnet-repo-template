9b1eec88e8842347e865410d9a229474b532c98e
Jonathon Marolf <jmarolf@microsoft.com>
2022-01-24 08:09:19 -0800

    adding stub template content

--- Build.cmd
+++ Build.cmd
@@ -3,4 +3,3 @@
 dotnet tool restore
 dotnet restore
 dotnet msbuild -graph -isolate -binaryLogger:artifacts/log/build.binlog
-dotnet pack --no-build --no-restore --nologo --output artifacts/packages
